2533c0af4ddececcc2ad3775941593374cb8c99e
codermuffin <codermuffin@gmail.com>
2023-05-08 10:31:58 +0000

    add login.sh

--- commit.sh
+++ commit.sh
@@ -1,4 +1,3 @@
-git config --global user.email "codermuffin@gmail.com"
-git config --global user.name "codermuffin"
+sh login.sh
 git add .
 git commit
---
+++ login.sh
@@ -0,0 +1,2 @@
+git config --global user.email "codermuffin@gmail.com"
+git config --global user.name "codermuffin"
